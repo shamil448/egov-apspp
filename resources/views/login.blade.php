23ed56b48fbb6f14e4b808069128eba6298ca4d9
Cristov Manurung <cristovrm2@gmail.com>
2024-09-19 18:57:45 +0700

    Menambah Login

---
+++ resources/views/login.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    
+    <title>Login Dashboard</title>
+</head>
+<body>
+    <div class="container py-5">
+        <div class="w-50 center border rounded px-3 py-3 mx-auto">
+        <h1>Login</h1>
+        @if($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach($errors->all() as $item) 
+                <li>{{ $item }}</li>
+                @endforeach
+            </ul>
+        </div>
+        @endif
+        <form action="" method="POST">
+            @csrf
+            <div class="mb-3">
+                <label for="email" class="form-label">Email</label>
+                <input type="email" value="{{ old('email') }}" name="email" class="form-control">
+            </div>
+            <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" name="password" class="form-control">
+            </div>
+            <div class="mb-3">   
+                <button name="submit" type="submit" class="btn btn-primary">Login</button>
+            </div>
+        </form>
+        @include('components.footer')
+    </div> 
+    </div>
+</body>
+</html>
