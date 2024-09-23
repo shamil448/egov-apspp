f531d78d7cea37f256fba78087e26be39c4b8e14
shamil yassin <ka9545182@gmail.com>
2024-09-23 23:47:00 +0700

    buat db dan model untuk "RW" dan pembaruan db users

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['rw', 'petugas', 'pemerintah']); // Role RW, Petugas & Pemerintah
             $table->rememberToken();
             $table->timestamps();
         });
