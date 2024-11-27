9e248495fa6d8ef033cc64cb01c747a8a543fa3b
Cristov Manurung <cristovrm2@gmail.com>
2024-11-27 18:31:14 +0700

    Tambah Petugas

--- app/Http/Controllers/PetugasController.php
+++ app/Http/Controllers/PetugasController.php
@@ -6,5 +6,21 @@ use Illuminate\Http\Request;
 
 class PetugasController extends Controller
 {
-    //
+    public function dashboard()
+    {
+        // Logika untuk menampilkan halaman dashboard
+        return view('petugas.dashboard');
+    }
+
+    public function jadwalRute()
+    {
+        // Logika untuk menampilkan halaman Jadwal dan Rute
+        return view('petugas.jadwalrute');
+    }
+
+    public function laporanTugas()
+    {
+        // Logika untuk menampilkan halaman Laporan Tugas
+        return view('petugas.laporantugas');
+    }
 }
